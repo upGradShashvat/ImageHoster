ce7b3ab6992cda0a0cdadec16a732756cae01563
Shashvat Mahendru <shashvatmahendru@shashvat-mahendru.attlocal.net>
2020-09-22 15:24:54 -0700

    Completed The Backend excluding connecting Database connectivity (Corrected)

---
+++ src/main/java/ImageHoster/controller/HomeController.java
@@ -0,0 +1,28 @@
+package ImageHoster.controller;
+
+import ImageHoster.model.Image;
+import ImageHoster.service.ImageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class HomeController {
+
+    //Write the code to inject the ImageService bean
+    @Autowired
+    private ImageService imageService;
+
+    @RequestMapping("/")
+    public String getAllImages(Model model) {
+        //Call getAllImages() method in ImageService class to get the list of all images
+        List<Image> images = imageService.getAllImages();
+        //Add the list of images in the model with the key as "images"
+        model.addAttribute("images",images);
+        return "index";
+    }
+}
